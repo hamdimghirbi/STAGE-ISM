9f7c2a0fb482bc388e91c80bcca5b9fb0706a716
Nedakh1307 <nedakhelifi@gmail.com>
2026-04-30 15:20:21 +0200

    Create HelloWorld test project with uv, virtual environment, and Rich package

---
+++ HelloWorld/helloworld.py
@@ -0,0 +1,18 @@
+from rich import print
+
+
+
+def main():
+    
+    print("Hello world")
+    
+    
+def main1():
+    
+    print("Hello, [bold magenta]World[/bold magenta]!", ":vampire:")
+
+# !!!!!!!!!!!             ENTRY POINT            !!!!!!!!!!!!
+if __name__ == "__main__":
+        main1()
+        
+        
